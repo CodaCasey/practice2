23ef03df221339d34d8ec268b184008a9199cf0a
Casey Jackson <ctjacks3@gmail.com>
2021-01-09 22:48:20 -0800

    finished module 3

--- components/src/ComponentDetail.js
+++ components/src/ComponentDetail.js
@@ -2,21 +2,21 @@ import React from 'react';
 import faker from 'faker';
 
 
-const ComponentDetail =()=>{
+const ComponentDetail =(props)=>{
   return (
 
       <div className="comment">
         <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.image()} />
+        <img alt="avatar" src={props.avi} />
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sam
+            {props.author}
           </a>
           <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
+            <span className="date">{props.time}</span>
           </div>
-          <div className="text">Nice blog post!</div>
+          <div className="text">{props.com}</div>
         </div>
       </div>
 
--- components/src/index.js
+++ components/src/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import ComponentDetail from './ComponentDetail.js'
+import ApprovalCard from './ApprovalCard.js'
 const App = ()=>{
   return (
     <div className="ui container comments">
-      <ComponentDetail />
+      <ApprovalCard>you dont want these problems</ApprovalCard>
+      <ApprovalCard>
+      <ComponentDetail
+       author="Jon"
+       time="today at 3:00"
+       com="sad boi hours"
+       avi={faker.image.image()} />
+      </ApprovalCard>
+      <ApprovalCard>
+      <ComponentDetail
+       author="Snow"
+       time="today at 5:00"
+       com="who want me"
+       avi={faker.image.image()} />
+       </ApprovalCard>
+       <ApprovalCard>
+      <ComponentDetail
+       author="Rules"
+       time="yesterday at 2:00"
+       com="i miss listening to X"
+       avi={faker.image.image()}/>
+       </ApprovalCard>
     </div>
   )
 };
